0ec1d3edc2abb8cd633edd8207769fb629b18a34
jungmtg <cardofang@gmail.com>
2016-03-06 09:33:42 +0800

    建立一個 MemberViewModel 類別與透過 ViewModel 建立兩個 Action 與相對應的 View 頁面

---
+++ MVC5Course/Controllers/TestController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5Course.Models;
+
+namespace MVC5Course.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            return View();
+        }
+		//Code Snippet shortcut:mvcaction4
+	    public ActionResult MemberProfile()
+	    {
+		    return View();
+	    }
+		//mvcpostaction4
+		[HttpPost]
+		public ActionResult MemberProfile(MemberViewModel data)
+		{
+			return View();
+		}
+		
+    }
+}
---
+++ MVC5Course/Models/MemberViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models
+{
+	public class MemberViewModel
+	{
+		public int Id{get; set;}
+
+		public string Name{get; set; }
+
+		[Required(ErrorMessage = "請設定有效的日期格式")]
+		public DateTime Birthday { get; set; }
+	}
+}
